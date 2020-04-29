1604da5a2de3185605b0d7a072e0080f852c1bb0
RayMik <raymond0876@gmail.com>
2020-04-29 15:08:21 +0100

    finnito

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ toggle_close_text = 'Close';
 
 toggle.addEventListener(
         'click',
-        function() {
+        function () {
                 nav.classList.toggle('open');
 
                 if (nav.classList.contains('open')) {
@@ -17,9 +17,44 @@ toggle.addEventListener(
         false
 );
 
-setTimeout(function() {
+setTimeout(function () {
         nav.classList.toggle('open');
 }, 800);
 
 // bg video speed
 document.getElementById('video_bg').playbackRate = 0.8;
+
+
+// button back to top
+
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+addEventListener("scroll", scrollFuntion);
+
+function scrollFuntion() {
+        if (pageYOffset > 300) { //Show backToTopButton on scroll- can use scrollY instead of pageYOffset, but thats not wrokign in IE explorer. 
+                if (!backToTopButton.classList.contains("btnEntrance")) {
+                        backToTopButton.classList.remove("btnExit"); //remove new class
+                        backToTopButton.classList.add("btnEntrance"); //add new class
+
+                        backToTopButton.style.display = "block";
+                }
+        }
+        else { //Hide backToTopButton
+                if (backToTopButton.classList.contains("btnEntrance")) {
+                        backToTopButton.classList.remove("btnEntrance"); //remove new class
+                        backToTopButton.classList.add("btnExit"); //add new class
+                        setTimeout(function () {
+                                backToTopButton.style.display = "none";
+
+                        }, 250);
+
+                }
+        }
+
+}
+
+backToTopButton.addEventListener("click", backToTop);
+function backToTop() {
+        scrollTo(0, 0);
+}
